b15f560776cb0ba4678cf49ca7a5d63f2d5d67c7
hawkinchina <free_hawk@sina.com>
2022-01-10 13:29:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-ex01
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-ex01)
---
+++ index.ts
@@ -0,0 +1,45 @@
+import './style.css';
+
+import { of, map, Observable, timer, fromEvent } from 'rxjs';
+
+//01. hello world.
+of('World')
+  .pipe(map((name) => `Hello, ${name}!`))
+  .subscribe(console.log);
+
+//02. 加入时钟
+const output = document.createElement('output');
+document.body.prepend(output);
+
+timer(0, 1000)
+  .pipe(map(() => new Date().toLocaleTimeString()))
+  .subscribe((time) => (output.textContent = time));
+
+//03. rxjs 初步使用
+// var button = document.querySelector('button');
+// button.addEventListener('click', () => console.log('Clicked!'));
+
+var button = document.querySelector('button');
+fromEvent(button, 'click').subscribe(() =>
+  console.log('Clicked!!!!!!!!!!!!!!!!!')
+);
+
+//04. observable原理
+
+//阶段一: 极简版; 同步输出..
+function simpleObservable(observer) {
+  for (let i = 0; i < 10; i++) {
+    observer.next(i);
+  }
+  observer.complete();
+}
+
+const observer = {
+  next: (value) => console.log(`next -> ${value}`),
+  error: () => {},
+  complete: () => console.log('complete'),
+};
+
+simpleObservable(observer);
+
+// Open the console in the bottom right to see results.
